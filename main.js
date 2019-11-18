59b6958f566c69387eaa9acc5bf141b548a642fb
Nick Sandison <nickstersand@gmail.com>
2019-11-18 16:02:36 -0600

    started redesign

--- main.js
+++ main.js
@@ -1,73 +1,44 @@
-function main() {
-    const canvas = document.getElementById("glCanvas");
-    //Initialize WebGL
-    const gl = canvas.getContext("webgl");
-    if(gl == null){
+$(function(){
+    let movingAll = false;
+    let movingOne = false;
+    let prevPosX = 0;
+    let prevPosY = 0;
+    $("main").mousedown(function(){movingAll = true});
+    $(".testDiv").mousedown(function(){movingOne = true});
+    $(document).mouseup(function(){movingOne = false; movingAll = false;});
+    $("main").mousemove(function(event){
+        if(movingAll && !movingOne){
+            let offsetX = event.pageX - prevPosX;
+            let offsetY = event.pageY - prevPosY;
+            $(".testDiv").each(function() {
+                $(this).css("top", parseInt($(this).css("top"), 10) + offsetY + "px");
+                $(this).css("left", parseInt($(this).css("left"), 10) + offsetX + "px");
+            });
+        }
+        prevPosX = event.pageX;
+        prevPosY = event.pageY;
+    });
+    $(".testDiv").draggable();
+    const mainCanvas = $("#mainCanvas").get(0);
+    const mainGL = mainCanvas.getContext("webgl");
+    if(mainGL == null){
         alert("Could not initialize WebGL");
         return;
     }
+    let mainShader = initShaderProgram(mainGL,vsSourceDefault,fsSourceDefault);
+    const mainBuffers = initBuffers(mainGL);
+    mainGL.useProgram(mainShader);
 
-    let defaultShader = initShaderProgram(gl, vsSourceDefault, fsSourceVoronoi);
-    let buffers = initBuffers(gl);
-    let uScreenSize = gl.getUniformLocation(defaultShader,"uScreenSize");
-    let uSeed = gl.getUniformLocation(defaultShader,"uSeed");
-    let uScale = gl.getUniformLocation(defaultShader,"uScale");
-    let uColor = gl.getUniformLocation(defaultShader,"uColor");
-    gl.useProgram(defaultShader);
-    gl.uniform2f(uScreenSize,canvas.width,canvas.height);
-    gl.uniform4f(uSeed, Math.random() * 200 + 50,Math.random() * 200 + 50,Math.random() * 200 + 50,Math.random() * 200 + 50);    gl.uniform1f(uScale, 20);
-    gl.uniform3f(uColor, 1,1,1);
-    const noiseR = document.getElementById("noiseR");
-    const noiseG = document.getElementById("noiseG");
-    const noiseB = document.getElementById("noiseB");
-    function noiseChange(){
-        gl.useProgram(defaultShader);
-        gl.uniform3f(uColor, noiseR.value, noiseG.value, noiseB.value);
-    }
-    noiseR.oninput = noiseChange;
-    noiseG.oninput = noiseChange;
-    noiseB.oninput = noiseChange;
-    const noiseScale = document.getElementById("noiseScale");
-    noiseScale.oninput = function(){
-        gl.useProgram(defaultShader);
-        gl.uniform1f(uScale, noiseScale.value);
-    }
-
-    document.getElementById("seed").onclick = function(){
-        gl.useProgram(defaultShader);
-        gl.uniform4f(uSeed, Math.random() * 200 + 50,Math.random() * 200 + 50,Math.random() * 200 + 50,Math.random() * 200 + 50);
-    }
-    $(".nodeSelect").change(function(){
-        const val = $(this).val();
-        let source = fsSourceDefault;
-        if(val == "value") {
-            source = fsSourceValue;
-        }else if(val == "worley"){
-            source = fsSourceVoronoi;
-        }else if(val == "perlin"){
-            source = fsSourcePerlin;
-        }
-        defaultShader = initShaderProgram(gl, vsSourceDefault, source);
-        uScreenSize = gl.getUniformLocation(defaultShader,"uScreenSize");
-        uSeed = gl.getUniformLocation(defaultShader,"uSeed");
-        uScale = gl.getUniformLocation(defaultShader,"uScale");
-        uColor = gl.getUniformLocation(defaultShader,"uColor");
-        gl.useProgram(defaultShader);
-        gl.uniform2f(uScreenSize,canvas.width,canvas.height);
-        gl.uniform4f(uSeed, Math.random() * 200 + 50,Math.random() * 200 + 50,Math.random() * 200 + 50,Math.random() * 200 + 50);    gl.uniform1f(uScale, 20);
-        gl.uniform3f(uColor, noiseR.value, noiseG.value, noiseB.value);
-        gl.uniform1f(uScale, noiseScale.value);
-    });
     let then = 0;
     function render(now){
         now *= 0.001;
         const deltaTime = now-then;
         then = now;
-        drawScene(gl,defaultShader,buffers);
+        drawScene(mainGL,mainShader,mainBuffers);
         requestAnimationFrame(render);
     }
     requestAnimationFrame(render);
-}
+});
 function initShaderProgram(gl, vsSource, fsSource){
     const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
     const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
@@ -84,7 +55,6 @@ function initShaderProgram(gl, vsSource, fsSource){
 
     return shaderProgram;
 }
-
 function loadShader(gl, type, source) {
     const shader = gl.createShader(type);
     gl.shaderSource(shader, source);
@@ -98,7 +68,6 @@ function loadShader(gl, type, source) {
 
     return shader;
 }
-
 function initBuffers(gl){
     const positionBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
@@ -114,7 +83,6 @@ function initBuffers(gl){
         position: positionBuffer
     };
 }
-
 function drawScene(gl, shaderProgram, buffers){
     gl.clearColor(0.0,0.0,0.0,1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
@@ -125,7 +93,5 @@ function drawScene(gl, shaderProgram, buffers){
         gl.enableVertexAttribArray(vertexPosition);
     }
     gl.useProgram(shaderProgram);
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4)
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 }
-
-window.onload = main;
